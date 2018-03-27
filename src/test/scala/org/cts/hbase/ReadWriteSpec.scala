c56e83cb6b5713b891f194c884e4f6b6316090be
Dilip Diwakar <dlpkmr98@gmail.com>
2018-03-27 18:50:03 +0200

    Update ReadWriteSpec.scala

--- src/test/scala/org/cts/hbase/ReadWriteSpec.scala
+++ src/test/scala/org/cts/hbase/ReadWriteSpec.scala
@@ -14,10 +14,35 @@ import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.client.ConnectionFactory
 import org.apache.hadoop.hbase.CellUtil
 import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.client.Result
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
 
-class ReadWriteSpec extends UnitSpec with MiniCluster {
+class ReadWriteSpec extends UnitSpec with MiniCluster with BeforeAndAfter with Serializable {
 
-  "A HBaseRDD" should "read/write to a Table all column families" in {
+  val tableName = "pricetest"
+  val columnFamily = "cftest"
+
+  before {
+    try {
+      htu.deleteTable(Bytes.toBytes(tableName))
+    } catch {
+      case e: Exception => {
+        println(" - no table +++++++++++" + tableName + " found")
+      }
+
+    }
+    println(" - creating table+++++++++++++++++++++ " + tableName)
+    htu.createTable(Bytes.toBytes(tableName), Bytes.toBytes(columnFamily))
+    println(" - created table+++++++++++++++++++++++")
+  }
+
+  after {
+    htu.deleteTable(Bytes.toBytes(tableName))
+    println(" - deleted table+++++++++++++++++++++++")
+  }
+
+  "A HBaseRDD" should "write to a hbase table" in {
 
     val config = htu.getConfiguration
 
@@ -35,12 +60,12 @@ class ReadWriteSpec extends UnitSpec with MiniCluster {
       TableName.valueOf(tableName),
       (putRecord) => {
         val put = new Put(putRecord._1)
-        putRecord._2.foreach((putValue) => put.add(putValue._1, putValue._2, putValue._3))
+        putRecord._2.foreach((putValue) => put.addColumn(putValue._1, putValue._2, putValue._3))
         put
       })
 
     val connection = ConnectionFactory.createConnection(config)
-    val table = connection.getTable(TableName.valueOf("t1"))
+    val table = connection.getTable(TableName.valueOf(tableName))
     try {
 
       val foo1 = Bytes.toString(CellUtil.cloneValue(table.get(new Get(Bytes.toBytes("1"))).
@@ -69,4 +94,79 @@ class ReadWriteSpec extends UnitSpec with MiniCluster {
 
   }
 
+  "A HBaseRDD" should "write/read to a hbase table" in {
+
+    val config = htu.getConfiguration
+
+    val rdd = sc.parallelize(Array(
+      (
+        Bytes.toBytes("row_key1"),
+        Array(
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c1"), Bytes.toBytes("NA")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c2"), Bytes.toBytes("US")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c3"), Bytes.toBytes("NYC")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c4"), Bytes.toBytes("EWR")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c5"), Bytes.toBytes("EU")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c6"), Bytes.toBytes("DE")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c7"), Bytes.toBytes("FRA")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c8"), Bytes.toBytes("FRA")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c9"), Bytes.toBytes("false")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c10"), Bytes.toBytes("")))),
+      (
+        Bytes.toBytes("row_key2"),
+        Array(
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c1"), Bytes.toBytes("OG")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c2"), Bytes.toBytes("OC")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c3"), Bytes.toBytes("OC1")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c4"), Bytes.toBytes("OA")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c5"), Bytes.toBytes("DG")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c6"), Bytes.toBytes("DC")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c7"), Bytes.toBytes("DC1")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c8"), Bytes.toBytes("DA")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c9"), Bytes.toBytes("bf")),
+          (Bytes.toBytes(columnFamily), Bytes.toBytes("c10"), Bytes.toBytes("no reason"))))))
+
+    val hbaseContext = new HBaseContext(sc, config)
+    hbaseContext.bulkPut[(Array[Byte], Array[(Array[Byte], Array[Byte], Array[Byte])])](
+      rdd,
+      TableName.valueOf(tableName),
+      (putRecord) => {
+        val put = new Put(putRecord._1)
+        putRecord._2.foreach((putValue) => put.addColumn(putValue._1, putValue._2, putValue._3))
+        put
+      })
+
+    val rdd_key = sc.parallelize(Array((Bytes.toBytes("row_key1")), (Bytes.toBytes("row_key2"))))
+
+    val getRdd = hbaseContext.bulkGet[Array[Byte], scala.collection.mutable.Map[String, String]](
+      TableName.valueOf(tableName),
+      2,
+      rdd_key,
+      record => {
+        new Get(record)
+      },
+      (result: Result) => {
+        val map = scala.collection.mutable.Map[String, String]()
+        if (result.listCells() != null) {
+          map += ("row_key" -> Bytes.toString(result.getRow))
+          val it = result.listCells().iterator()
+          while (it.hasNext) {
+            val cell = it.next()
+            val q = Bytes.toString(CellUtil.cloneQualifier(cell))
+            map += (q -> Bytes.toString(CellUtil.cloneValue(cell)))
+          }
+        }
+        map
+      })
+
+    println("==================>>>>>>>>>>>>>>>>>>>>>>>")
+    getRdd.collect().foreach(println)
+
+    val zipRdd = getRdd.filter(!_.isEmpty).map(x => x.toList.sortBy(_._1).unzip)
+    val schema = StructType(zipRdd.first()._1.map(k => StructField(k, StringType, nullable = false)))
+    val rows = zipRdd.map(_._2).map(x => (Row(x: _*)))
+
+    val priceDF = sqlCtx.createDataFrame(rows, schema)
+    priceDF.show()
+  }
 }
